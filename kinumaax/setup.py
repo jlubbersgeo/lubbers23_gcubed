3ec67b9c0b603fef1f1b4cb84261721839661e09
Jordan Lubbers <jelubber@gmail.com>
2023-05-03 08:45:28 -0800

    updated gitignore and setup

--- kinumaax/setup.py
+++ kinumaax/setup.py
@@ -5,9 +5,9 @@ Created on Sat Jan  1 11:39:09 2022
 @author: jlubbers
 """
 
-from setuptools import setup, find_packages
-
 from os import path
+
+from setuptools import find_packages, setup
 
 this_directory = path.abspath(path.dirname(__file__))
 
@@ -17,14 +17,14 @@ with open(path.join(this_directory, 'source', 'kigusiq', '_version.py'), encodin
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-setup(name = 'kigusiq',
+setup(name = 'kinumaax',
       version = __version__,
       author = 'Jordan',
       author_email = 'jelubber@gmail.com',
       description = 'kiqusiq',
       long_description = long_description,
       long_description_content_type = 'text/markdown',
-      url = 'https://github.com/jlubbersgeo/kigusiq',
+      url = 'https://github.com/jlubbersgeo/lubbers23_gcubed',
       package_dir = {'':'source'},
       packages = find_packages(where = 'source'),
       install_requires = [
